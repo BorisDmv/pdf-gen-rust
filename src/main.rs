9f61795400d84a6297ce5a4c2153af0031693442
Boris <borisdimitrov1998@gmail.com>
2023-11-10 12:04:50 +0200

    Added query_map for cases where you need to pass an argument to the query

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::BufWriter;
 
 use mysql::prelude::*;
 use mysql::{self, OptsBuilder};
+use std::env;
 
 fn main() {
     let server = "localhost";
@@ -14,31 +15,47 @@ fn main() {
 
     // Build options for the database connection
     let opts = OptsBuilder::new()
-            .ip_or_hostname(Some(server))
-            .db_name(Some(database))
-            .user(Some(username))
-            .pass(Some(password))
-            .tcp_port(port)
-            .ssl_opts(None);
+        .ip_or_hostname(Some(server))
+        .db_name(Some(database))
+        .user(Some(username))
+        .pass(Some(password))
+        .tcp_port(port)
+        .ssl_opts(None);
 
     // Connect to the database
     let pool = mysql::Pool::new(mysql::Opts::from(opts)).unwrap();
     let mut conn = pool.get_conn().unwrap();
 
-    let result: Vec<(String, String, String)> = conn
-            .query("SELECT id, client, quarter FROM taxable_wages")
-            .unwrap();
-    
-    
+    let args: Vec<String> = env::args().collect();
+
+    // Check if an argument is provided
+    if args.len() < 2 {
+        eprintln!("Usage: {} <id>", args[0]);
+        std::process::exit(1);
+    }
+
+    // Parse the ID from the command line argument
+    let id: i32 = args[1].parse().expect("Invalid ID");
+
+    let query_str = format!("SELECT id, name, role FROM users WHERE id = {}", id);
+
+    let result: Vec<(i32, String, String)> = conn
+        .query_map(query_str, |(id, name, role)| (id, name, role))
+        .unwrap();
+
+    //This is if you want to pass a single argument in the query
+
+    //let result: Vec<(String, String, String)> = conn
+    //        .query("SELECT id, client, quarter FROM taxable_wages").unwrap();
+
     const WIDTH: f64 = 210.0;
     const HEIGHT: f64 = 297.0;
     const COL1_X: f64 = 10.0;
-    const COL2_X: f64 = 120.0;
+    //const COL2_X: f64 = 120.0;
     const ROW1_Y: f64 = 267.0;
-    const ROW2_Y: f64 = 1.0;
-    const ROW3_Y: f64 = 7.0;
-    
-    
+    //const ROW2_Y: f64 = 1.0;
+    //const ROW3_Y: f64 = 7.0;
+
     const MAX_ITEMS_PER_PAGE: usize = 20; // Set the maximum number of items per page
     let (doc, mut current_page, layer1) =
         PdfDocument::new("PDF_Document_title", Mm(WIDTH), Mm(HEIGHT), "Layer 1");
@@ -46,7 +63,7 @@ fn main() {
     let mut y_position = ROW1_Y; // Initial y-position
     let mut item_count = 0;
 
-    for (id, client, quarter) in &result {
+    for (id, name, role) in &result {
         if item_count >= MAX_ITEMS_PER_PAGE {
             let (new_page, _) = doc.add_page(Mm(WIDTH), Mm(HEIGHT), "New Page");
             current_page = new_page;
@@ -57,21 +74,26 @@ fn main() {
         let current_layer = doc.get_page(current_page).get_layer(layer1);
 
         // Concatenate the text and call add_text for each item, incrementing y_position
-        let text = format!("ID: {}, Client: {}, Quarter: {}", id, client, quarter);
-        add_text(&current_layer, &font, COL1_X, y_position, &text,16.0);
+        let text = format!("ID: {}, Name: {}, Role: {}", id, name, role);
+        add_text(&current_layer, &font, COL1_X, y_position, &text, 16.0);
 
         // Increment the y_position and item_count
         y_position -= 10.0; // Adjust this value for the desired spacing
         item_count += 1;
     }
-        
-        
+
     doc.save(&mut BufWriter::new(File::create("file_name.pdf").unwrap()))
-    .unwrap();
+        .unwrap();
 }
 
-
-fn add_text(current_layer: &PdfLayerReference, font: &IndirectFontRef, x: f64, y: f64, text: &str, font_size: f64) {
+fn add_text(
+    current_layer: &PdfLayerReference,
+    font: &IndirectFontRef,
+    x: f64,
+    y: f64,
+    text: &str,
+    font_size: f64,
+) {
     current_layer.begin_text_section();
     current_layer.set_font(font, font_size);
     current_layer.set_text_cursor(Mm(x), Mm(y));
